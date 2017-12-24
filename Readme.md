0878524aea4b6bd77e55626b3bb1b6adbdb23798
hisham yasin <hisham.yasin@il.ibm.com>
2017-12-24 10:13:08 +0200

    refactoring and support command line

--- Readme.md
+++ Readme.md
@@ -1,17 +1,23 @@
 # Goal
-The goal of this project is to sign a certificate with a CA authority.
+The goal of this project is to sign a certificate Chain.
 The best use-case for signing a certificate with a CA without paying any nickel or waiting for an authority to sign it
 is for testing purpuses.
 
-#Environment
+# Environment
+You can use the API manually or with command line as depicted below.
+
+## Run Web Server API
 This project will run a web server. With a WebUI Client you can create and download your certificates.
 The backend will use the local root.crt and intermediate.crt to sign the requested certificate submitted by the client.
 
-##Scenarios
-If the root and the intermediate certificates are not available, they will be generated automatically.
-IF the parent chain certificates are available, then the server will use them to sign your certificates.
+## Run  Manually
+The script certificateAPI.py can be run manually in order to create the certificate
 
-##Dependencies
+## Scenarios
+* If the root and the intermediate certificates are not available, they will be generated automatically.
+* If the parent chain certificates are available, then the server will use them to sign your certificates.
+
+## Dependencies
 ```
 apt-get install python-dev
 pip install pyopenssl
@@ -19,16 +25,76 @@ pip install --egg M2Crypto
 pip install Flask
 ```
 
-# Install
+# WebUI
 You need to install the libraries as mentioned in the makefile.
 Afterward, You should run the server server.py
-
 ``
 python server.py
 ``
+
 Login to the website https://localhost:8081
 
 There you should fill your certificate details
+
+# CommandLine
+
+```
+python certofocateAPI.py --ca --cert --cn localhost
+```
+
+The possible flags are:
+
+```
+python certificateAPI.py --help
+usage: certificateAPI.py [-h] [--root-c ROOT_ISSUER_C]
+                         [--root-cn ROOT_ISSUER_CN]
+                         [--inter-c INTERMEDIATE_ISSUER_C]
+                         [--inter-cn INTERMEDIATE_ISSUER_CN] [--cn CERT_CN]
+                         [--c CERT_C] [--root-key-file ROOT_KEY_FILE]
+                         [--root-crt-file ROOT_CRT_FILE]
+                         [--inter-key-file INTERMEDIATE_KEY_FILE]
+                         [--inter-crt-file INTERMEDIATE_CRT_FILE]
+                         [--key-file KEY_FILE] [--crt-file CRT_FILE]
+                         [--version] [--clean] [--cert] [--ca]
+
+my flags
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --root-c ROOT_ISSUER_C
+                        The root issuer country
+  --root-cn ROOT_ISSUER_CN
+                        The root issuer common name
+  --inter-c INTERMEDIATE_ISSUER_C
+                        The intermediate country name
+  --inter-cn INTERMEDIATE_ISSUER_CN
+                        The intermediate issuer common name
+  --cn CERT_CN          Common name of the certificate
+  --c CERT_C            Country of the certificate
+  --root-key-file ROOT_KEY_FILE
+                        The output root certificate file. if file exists, it
+                        will be overwritten
+  --root-crt-file ROOT_CRT_FILE
+                        The output root key certificate file. if file exists,
+                        it will be overwritten
+  --inter-key-file INTERMEDIATE_KEY_FILE
+                        The output intermediate certificate file. if file
+                        exists, it will be overwritten
+  --inter-crt-file INTERMEDIATE_CRT_FILE
+                        The output intermediate key certificate file. if file
+                        exists, it will be overwritten
+  --key-file KEY_FILE   The output certificate file. if file exists, it will
+                        be overwritten
+  --crt-file CRT_FILE   The output key certificate file. if file exists, it
+                        will be overwritten
+  --version             show program's version number and exit
+  --clean               Clean output files
+  --cert                Create certificate
+  --ca                  Create root certificate - if intermediate is not
+                        available, it will be created
+
+
+```
 
 # Example
 You can use up and running example by going to https://thunderclouding.ddns.net.
--- certificateAPI.py
+++ certificateAPI.py
@@ -25,18 +25,49 @@ THE SOFTWARE.
 from OpenSSL.crypto import X509
 import os, time, base64, sys
 from M2Crypto import X509, EVP, RSA, ASN1, m2
+import argparse
 
-root_issuer_c = "IL"
-root_issuer_cn = "Hisham Authoritah"
-intermediate_issuer_c = "IL"
-intermediate_issuer_cn = "Hisham Intermediate Authoritah"
+# Initialize command line
+parser = argparse.ArgumentParser(description='my flags')
+parser.add_argument('--root-c',action='store',dest='root_issuer_c', help='The root issuer country',default=['IL'], nargs=1, required=False)
+parser.add_argument('--root-cn',action='store',dest='root_issuer_cn', help='The root issuer common name',default=['Hesham Authoritah'], nargs=1, required=False)
+parser.add_argument('--inter-c',action='store',dest='intermediate_issuer_c', help='The intermediate country name',default=['IL'], nargs=1, required=False)
+parser.add_argument('--inter-cn',action='store',dest='intermediate_issuer_cn', help='The intermediate issuer common name',default=['Hisham Intermediate Authoritah'], nargs=1, required=False)
+parser.add_argument('--cn',action='store',dest='cert_cn', help='Common name of the certificate',default=['localhost'], nargs=1, required=False)
+parser.add_argument('--c',action='store',dest='cert_c', help='Country of the certificate',default=['IL'], nargs=1, required=False)
+parser.add_argument('--root-key-file',action='store',dest='root_key_file', help='The output root certificate file. if file exists, it will be overwritten',default=['root.key'], nargs=1, required=False)
+parser.add_argument('--root-crt-file',action='store',dest='root_crt_file', help='The output root key certificate file. if file exists, it will be overwritten',default=['root.crt'], nargs=1, required=False)
+parser.add_argument('--inter-key-file',action='store',dest='intermediate_key_file', help='The output intermediate certificate file. if file exists, it will be overwritten',default=['inter.key'], nargs=1, required=False)
+parser.add_argument('--inter-crt-file',action='store',dest='intermediate_crt_file', help='The output intermediate key certificate file. if file exists, it will be overwritten',default=['inter.crt'], nargs=1, required=False)
+parser.add_argument('--key-file',action='store',dest='key_file', help='The output certificate file. if file exists, it will be overwritten',default=['cert.key'], nargs=1, required=False)
+parser.add_argument('--crt-file',action='store',dest='crt_file', help='The output key certificate file. if file exists, it will be overwritten',default=['cert.crt'], nargs=1, required=False)
+parser.add_argument('--version', action='version', version=' 1.0')
+parser.add_argument('--clean',dest='clean', help='Clean output files', required=False, action="store_true")
+parser.add_argument('--cert',dest='create_cert', help='Create certificate', required=False, action="store_true")
+parser.add_argument('--ca', dest='create_root', help='Create root certificate - if intermediate is not available, it will be created', required=False, action="store_true")
+
+args = parser.parse_args()
+
+# Global params
+root_issuer_c = args.root_issuer_c[0]
+root_issuer_cn = args.root_issuer_cn[0]
+intermediate_issuer_c = args.intermediate_issuer_c[0]
+intermediate_issuer_cn = args.intermediate_issuer_cn[0]
+cert_cn = args.cert_cn[0]
+cert_c = args.cert_c[0]
+cert_key_file = args.key_file[0]
+cert_file = args.crt_file[0]
+root_key_file = args.root_key_file [0]
+root_crt_file = args.root_crt_file[0]
+intermediate_key_file = args.intermediate_key_file[0]
+intermediate_crt_file = args.intermediate_crt_file[0]
 
 
 def callback(*args):
     pass
 
 
-def mkreq(bits, ca=0, cn="exhesham ca group", c="IL"):
+def mkreq(bits, ca=0, cn=cert_cn, c=cert_c):
     pk = EVP.PKey()
     x = X509.Request()
     rsa = RSA.gen_key(bits, 65537, callback)
@@ -85,6 +116,11 @@ def generate_and_sign_cert(req, pk, sign_key, issuer_cn, issuer_c):
 
 
 def create_root_cert():
+    if os.path.exists(root_crt_file):
+        os.remove(root_crt_file)
+    if os.path.exists(root_key_file):
+        os.remove(root_key_file)
+
     req, pk = mkreq(4096, ca=1, cn=root_issuer_cn, c=root_issuer_c)
     cert, pk, pkey = generate_and_sign_cert(req, pk, sign_key=pk, issuer_cn=root_issuer_cn, issuer_c=root_issuer_c)
     if m2.OPENSSL_VERSION_NUMBER >= 0x0090800fL:
@@ -98,49 +134,79 @@ def create_root_cert():
     else:
         return None, None, None
         # TODO:self.assertRaises(AttributeError, cert.check_ca)
-    cert.save("root.crt")
-    pk.save_key("root.key", cipher=None)
+    cert.save(root_crt_file)
+    pk.save_key(root_key_file, cipher=None)
     return cert, pk, pkey
 
 
 def create_intermediate_cert(root_pkey=None):
-    if not os.path.exists("root.key"):
+
+    if not os.path.exists(root_key_file):
         return 1, 'create root certificate'
+
     if not root_pkey:
-        root_pkey = EVP.load_key("root.key")
+        root_pkey = EVP.load_key(root_key_file)
+    # Clean intermediate cert
+    if os.path.exists(intermediate_crt_file):
+        os.remove(intermediate_crt_file)
+    if os.path.exists(intermediate_key_file):
+        os.remove(intermediate_key_file)
+
     req, pk = mkreq(2048, ca=1, cn=intermediate_issuer_cn, c=intermediate_issuer_c)
     cert, pk, pkey = generate_and_sign_cert(req, pk, sign_key=root_pkey, issuer_cn=root_issuer_cn,
                                             issuer_c=root_issuer_c)
-    pk.save_key("inter.key", cipher=None)
-    cert.save("inter.crt")
+    pk.save_key(intermediate_key_file, cipher=None)
+    cert.save(intermediate_crt_file)
     return cert, pk, pkey
-
-
-
-
 
 def create_chain():
     '''Create certificate CA chain made of root and intermediate chain'''
-    if os.path.exists('root.crt'):
-        os.remove('root.crt')
-    if os.path.exists('root.key'):
-        os.remove('root.key')
-    if os.path.exists('inter.crt'):
-        os.remove('inter.crt')
-    if os.path.exists('inter.key'):
-        os.remove('inter.key')
     create_root_cert()
     create_intermediate_cert()
 
+def clean_files():
+    remove_file(root_crt_file)
+    remove_file(root_key_file)
+    remove_file(intermediate_crt_file)
+    remove_file(intermediate_key_file)
+    remove_file(cert_file)
+    remove_file(cert_key_file)
 
-def sign_cert(cn, c):
-    if not os.path.exists("root.crt") \
-            or not os.path.exists("root.key") \
-            or not os.path.exists("inter.crt") \
-            or not os.path.exists("inter.key"):
+def sign_cert(cn = cert_cn, c = cert_c):
+    if not os.path.exists(root_crt_file) \
+            or not os.path.exists(root_key_file) \
+            or not os.path.exists(intermediate_crt_file) \
+            or not os.path.exists(intermediate_key_file):
         create_chain()
-    inter_pkey = EVP.load_key("inter.key")
+    inter_pkey = EVP.load_key(intermediate_key_file)
     req, pk = mkreq(2048, ca=1, cn=cn, c=c)
     cert, pk, pkey = generate_and_sign_cert(req, pk, sign_key=inter_pkey, issuer_cn=intermediate_issuer_cn,
                                             issuer_c=intermediate_issuer_c)
     return cert, pk, pkey
+
+def save_to_text_file(text, filename):
+    remove_file(filename)
+    with open(filename, "w") as text_file:
+        text_file.write(text)
+
+def remove_file(filename):
+    if os.path.exists(filename):
+        os.remove(filename)
+
+def create_ca_signed_certificiate():
+    signed_cert, key, pkey = sign_cert()
+    save_to_text_file(signed_cert.as_pem(), cert_file)
+    save_to_text_file(str(key.as_pem(cipher=None)), cert_key_file)
+
+if __name__ == '__main__':
+    if args.clean:
+        print "Will clean files"
+        clean_files()
+    if args.create_root:
+        print "Will create root certificate"
+        create_root_cert()
+    if args.create_cert:
+        print "Will create a certificate"
+        create_ca_signed_certificiate()
+
+
